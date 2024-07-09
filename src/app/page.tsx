e27b9b9c834758373ffc71b8344a6edf0d34dfd8
Oscar Cheung <cheung.oscar@gmail.com>
2024-07-09 14:44:45 -0700

    table now reads in from firebase database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+'use client'
+import React, { useEffect, useState, DO_NOT_USE_OR_YOU_WILL_BE_FIRED_CALLBACK_REF_RETURN_VALUES } from 'react';
 import { 
   Table,
   TableBody,
@@ -10,19 +11,22 @@ import {
 } from '@/components/ui/table'
 import { strict } from 'assert';
 //import salaries from '@/resources/TestData.json';
-import salaries from '@/resources/MasterData.json';
+//import salaries from '@/resources/MasterData.json';
 // import from firebase
-
-/* interface Salary {
-  emailAddress?: string,
-  location?: string,
-  date?: string | number | Date,
-  specialty?: string,
-  experience?: number,
-  pay?: number
-} */
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from "@/resources/firebase";
+import firebase from 'firebase/app';
 
 interface Salary {
+  emailAddress: string,
+  location: string,
+  date: firebase.firestore.Timestamp,
+  specialty: string,
+  experience: number,
+  pay: number
+}
+
+/* interface Salary {
   "Month Year"?: string | number,
   State?: string,
   "City "?: string | number,
@@ -31,14 +35,14 @@ interface Salary {
   "Hourly Base Pay (Diff not included)"?: string | number,
   "Shift Diff Amount (if any) "?: string | number,
   "Type Of Shift Diff (nights, Baylor, Critical Care, Etc) "?: string | number
-}
+} */
 
-async function getSalaries(): Promise<Salary[]> {
+/* async function getSalaries(): Promise<Salary[]> {
   //const result = await fetch('http://localhost:4000/salaries')
   const results = salaries;
 
   return results;
-}
+} */
 
 /* const fetchSalaries = async (): Promise<Salary[]> => {
   const querySnapshot = await getDocs(collection(db, 'salaries'));
@@ -47,10 +51,26 @@ async function getSalaries(): Promise<Salary[]> {
   return salaries;
 } */
 // location, data, specialty, experience, pay
-export default async function Home() {
-  const salaries = await getSalaries()
+export default function Home() {
+  //const salaries = await getSalaries()
+  const [salaries, setSalaries] = useState<Salary[]>([]);
 
-  if (!salaries) {
+  useEffect(() => {
+    const fetchSalaries = async () => {
+      try {
+        const querySnapshot = await getDocs(collection(db, 'salaries'));
+        const fetchedSalaries = querySnapshot.docs.map(doc => doc.data() as Salary);
+        setSalaries(fetchedSalaries);
+      } catch (error) {
+        console.error('Error fetching salaries:', error);
+        // Handle error state if needed
+      }
+    };
+
+    fetchSalaries();
+  }, []);
+
+  if (!salaries || salaries.length === 0) {
     return <div>Loading...</div>;
   }
 
@@ -66,20 +86,16 @@ export default async function Home() {
               <TableHead className="text-left table-cell table-header-sticky">Experience</TableHead>
               <TableHead className="text-left table-cell table-header-sticky">Speciality</TableHead>
               <TableHead className="text-left table-cell table-header-sticky">Pay</TableHead>
-              <TableHead className="text-left table-cell table-header-sticky">Shift Diff Amount (if any)</TableHead>
-              <TableHead className="text-left table-cell table-header-sticky">Type of Shift Diff (nights, Baylor, Critical Care, etc)</TableHead>
             </TableRow>
           </TableHeader>
           <TableBody>
           {salaries.map((salary, index) => (
             <TableRow key={index} className={index % 2 === 0 ? 'table-row-even' : 'table-row-odd'}>
-              <TableCell className="text-left table-cell">{salary["Month Year"]}</TableCell>
-              <TableCell className="text-left table-cell text-nowrap">{`${String(salary["City "] || "").trim()}, ${salary.State}`}</TableCell>
-              <TableCell className="text-left table-cell">{salary["Years of Experience "]}</TableCell>
-              <TableCell className="text-left table-cell">{salary["Specialty (Cardiac, ER, GI, L&D, etc)"]}</TableCell>
-              <TableCell className="text-left table-cell">${salary["Hourly Base Pay (Diff not included)"]}/hr</TableCell>
-              <TableCell className="text-left table-cell">{salary["Shift Diff Amount (if any) "]}</TableCell>
-           <  TableCell className="text-left table-cell">{salary["Type Of Shift Diff (nights, Baylor, Critical Care, Etc) "]}</TableCell>
+              <TableCell className="text-left table-cell">{salary.date.toDate().toLocaleDateString()}</TableCell>
+              <TableCell className="text-left table-cell text-nowrap">{`${String(salary.location|| "").trim()}`}</TableCell>
+              <TableCell className="text-left table-cell">{salary.experience}</TableCell>
+              <TableCell className="text-left table-cell">{salary.specialty}</TableCell>
+              <TableCell className="text-left table-cell">${salary.pay}/hr</TableCell>
           </TableRow>
           ))}
           </TableBody>
