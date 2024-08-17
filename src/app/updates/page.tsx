033ccf8122808fe232bd51aeb78096b94038c5e3
maffew-tan <71350846+maffew-tan@users.noreply.github.com>
2024-08-17 09:02:42 -0700

    test

--- src/app/updates/page.tsx
+++ src/app/updates/page.tsx
@@ -7,7 +7,7 @@ export default function SubmitSalaryPage() {
                 <div className="header-content mb-5 font-american-typewriter">
                     <p>Today&apos;s date: {currentDate}</p>
                     <p className="mb-8">This will be updated as changes are made to the site.</p>
-                    <p>[8/12/24] 5 reports submitted (California x3, Arizona, Florida)</p>
+                    <p>[8/12/24] 5 reports submitted (California x3, Arizona, Florida.)</p>
                     <p>[8/7/24] 3 reports submitted (Washington, Oregon, California)</p>
                     <p>[8/4/24] 4 reports submitted (Colorado, Nevada, California x2)</p>
                     <p>[7/28/24] 5 reports submitted (Alabama, California x4, New York). First update 🎉</p>
