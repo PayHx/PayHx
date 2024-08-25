ec5bc64ae88dc945ceb083d903ef472ec482dcd0
maffew-tan <71350846+maffew-tan@users.noreply.github.com>
2024-08-24 17:10:22 -0700

    Update page.tsx
    Added Kentucky

--- src/app/updates/page.tsx
+++ src/app/updates/page.tsx
@@ -7,12 +7,14 @@ export default function SubmitSalaryPage() {
                 <div className="header-content mb-5 font-american-typewriter">
                     <p>Today&apos;s date: {currentDate}</p>
                     <p className="mb-8">This will be updated as changes are made to the site.</p>
+                    <p>[8/24/24] 1 report submitted</p>
+                    <p> Rehabilitation Nursing Salary. Lawrence, Kentucky (YOE: 2, $32.08/hr) </p>
                     <p>[8/17/24] 5 reports submitted</p>
-                    <p>- Med surg nursing salary Tempe Arizona (YOE: 8, $55.00/hr) </p>
-                    <p>- Cardiac nursing salary Phoenix, Arizona (YOE: 3, $55.00/hr)</p>
-                    <p>- Pediatric nursing salary Phoenix, Arizona (YOE: 3, $35.60/hr)</p>
-                    <p>- ICU nursing salary Roseville, California (YOE: 7, $75.00/hr)</p>
-                    <p>- OR nursing salary Oakland, California (YOE: 6.5, $92.00/hr)</p>
+                    <p>- Med surg nursing salary. Tempe Arizona (YOE: 8, $55.00/hr) </p>
+                    <p>- Cardiac nursing salary. Phoenix, Arizona (YOE: 3, $55.00/hr)</p>
+                    <p>- Pediatric nursing salary. Phoenix, Arizona (YOE: 3, $35.60/hr)</p>
+                    <p>- ICU nursing salary Roseville. California (YOE: 7, $75.00/hr)</p>
+                    <p>- OR nursing salary Oakland. California (YOE: 6.5, $92.00/hr)</p>
                     <p>[8/12/24] 5 reports submitted (California x3, Arizona, Florida)</p>
                     <p>[8/7/24] 3 reports submitted (Washington, Oregon, California)</p>
                     <p>[8/4/24] 4 reports submitted (Colorado, Nevada, California x2)</p>
