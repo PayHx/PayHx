cb2eb0b2bb52173d2dda0272dad1037709863480
Oscar Cheung <cheung.oscar@gmail.com>
2024-09-01 23:06:39 -0700

    separated columns also

---
+++ src/components/columns.tsx
@@ -0,0 +1,140 @@
+"use client"
+
+import { ColumnDef } from "@tanstack/react-table"
+import { Button } from "@/components/ui/button"
+import { ArrowUpDown } from "lucide-react"
+
+type Payment = {
+    date: string
+    state: string,
+    city: string,
+    experience: number
+    specialty: string
+    pay: number
+    shiftType: string
+    shiftDiff: number | null | undefined
+  }
+  
+  export const columns: ColumnDef<Payment>[] = [
+    {
+      accessorKey: "date",
+      header: ({ column }) => {
+        return (
+          <Button
+            variant="ghost"
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+            Date
+            <ArrowUpDown className="ml-2 h-4 w-4" />
+          </Button>
+        )
+      }
+    },
+    {
+      accessorKey: "location",
+      header: ({ column }) => {
+        return (
+          <Button
+            variant="ghost"
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+            Location
+            <ArrowUpDown className="ml-2 h-4 w-4" />
+          </Button>
+        )
+      }
+    },
+    {
+      accessorKey: "experience",
+      header: ({ column }) => {
+        return (
+          <Button
+            variant="ghost"
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+            className="w-full flex justify-center"
+          >
+            Experience
+            <ArrowUpDown className="ml-2 h-4 w-4" />
+          </Button>
+        )
+      },
+      cell: ({ row }) => {
+        return <div className="text-center font-medium">{row.getValue("experience")}</div>
+      },
+    },
+    {
+      accessorKey: "specialty",
+      header: ({ column }) => {
+        return (
+          <Button
+            variant="ghost"
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+            Specialty
+            <ArrowUpDown className="ml-2 h-4 w-4" />
+          </Button>
+        )
+      },
+    },
+    {
+      accessorKey: "shiftType",
+      header: ({ column }) => {
+        return (
+          <Button
+            variant="ghost"
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+            Shift Type
+            <ArrowUpDown className="ml-2 h-4 w-4" />
+          </Button>
+        )
+      },
+    },
+    {
+      accessorKey: "shiftDiff",
+      header: ({ column }) => {
+        return (
+          <Button
+            variant="ghost"
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+            Shift Diff
+            <ArrowUpDown className="ml-2 h-4 w-4" />
+          </Button>
+        )
+      },
+      cell: ({ row }) => {
+        const shiftDiff = row.getValue("shiftDiff");
+    
+        return (
+          <div className="text-right" style={{ paddingRight: '100px' }}>
+            {shiftDiff != null ? shiftDiff : ""}
+          </div>
+        );
+      },
+    },
+    {
+      accessorKey: "pay",
+      header: ({ column }) => {
+        return (
+          <Button
+            variant="ghost"
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+            className="w-full flex justify-end"
+          >
+            Pay
+            <ArrowUpDown className="ml-2 h-4 w-4" />
+          </Button>
+        )
+      },
+      cell: ({ row }) => {
+        const amount = parseFloat(row.getValue("pay"))
+        const formatted = new Intl.NumberFormat("en-US", {
+          style: "currency",
+          currency: "USD",
+        }).format(amount)
+   
+        return <div className="text-right font-medium">{formatted}/hr</div>
+      },
+    },
+  ]
