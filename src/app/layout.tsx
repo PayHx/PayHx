0ce735419c42c7e5dee8edfadf868975a42a7950
Oscar Cheung <31262167+Ahhsca@users.noreply.github.com>
2025-01-03 13:52:42 -0800

    added separator

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Link from 'next/link';
 import { Toaster } from "@/components/ui/toaster";
+import { MainSeparator } from "@/components/separator";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,24 +20,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <nav>
-          <div className="navbar">
-            <Link href="/">
-              <img src="/logo.png" alt="Logo" className="logo"/>
-            </Link>
-            <div className="nav-links">
-              <Link legacyBehavior href="/updates">
-                <a className="font-american-typewriter">Updates</a>
+        <div className="container mx-auto py-10">
+          <div className="flex items-center justify-between mb-6">
+              <Link href="/">
+                <img src="/logo.png" alt="Logo" className="logo"/>
               </Link>
-              <Link legacyBehavior href="/about-us">
-                <a className="font-american-typewriter">About Us</a>
-              </Link>
-              <Link legacyBehavior href="/submit-salary">
-                <a className="font-american-typewriter">Submit Salary</a>
-              </Link>
-            </div>
+              <MainSeparator />
+           </div>
           </div>
-        </nav>
         {children}
         <Toaster/>
       </body>
---
+++ src/components/separator.tsx
@@ -0,0 +1,37 @@
+import { Separator } from "@/components/ui/separator"
+
+import { Button } from "@/components/ui/button"
+import Link from "next/link"
+
+export function MainSeparator() {
+    return (
+        <div>
+          <div className="space-y-1 font-american-typewriter">
+            <h4 className="text-xl font-bold font-underline font-medium leading-none">PayHx</h4>
+            <p className="text-sm text-muted-foreground">
+              Real Salaries, Real People.
+            </p>
+          </div>
+          <Separator className="my-4"/>
+          <div className="flex h-5 items-center space-x-4 text-xl font-american-typewriter">
+            <Link href="/updates">
+                <Button variant="link">
+                    Updates
+                </Button>
+            </Link>
+            <Separator orientation="vertical" />
+            <Link href="/about-us">
+                <Button variant="link">
+                    About Us
+                </Button>
+            </Link>
+            <Separator orientation="vertical" />
+            <Link href="/submit-salary">
+                <Button variant="link">
+                    Submit Salary
+                </Button>
+            </Link>
+          </div>
+        </div>
+      )
+}
